835f18bd93c5facebc1e6358eb502ccc72b64d94
Alexey Piltenko <ukr.top@ukr.net>
2022-05-12 17:13:02 +0300

    Works))

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 
 const hStyle = { color: 'green' };
 export default function App() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [images, setImages] = useState('');
+  const [url, setUrl] = useState(
+    'https://api.memegen.link/images/aag/foo/bar.png',
+  );
   const meme = {
     name: 'Meme generator',
     profileImageUrl: 'https://api.memegen.link/images/aag/foo/bar.png',
@@ -11,30 +18,61 @@ export default function App() {
     { id: 2, pic: 'ackbar' },
     { id: 3, pic: 'afraid' },
     { id: 4, pic: 'agnes' },
+    { id: 5, pic: 'doge' },
   ];
 
   return (
     <div className="App">
-      <img src={meme.profileImageUrl} alt="My meme generator" />
+      <img src={url} alt="My meme generator" />
       <h1 style={hStyle}>{meme.name}</h1>
-      {templates.map((template) => (
-        <select key={template.id}>
-          {template.id}
-          {template.pic}
+      <label>
+        Choose a template{' '}
+        <select
+          value={images}
+          onChange={(event) => setImages(event.currentTarget.value)}
+        >
+          {templates.map((template) => (
+            <option value={template.pic} key={template.id}>
+              {template.pic}
+            </option>
+          ))}
         </select>
-      ))}
+      </label>
+      <br />
+      <br />
 
       <label>
         Top meme Text
         <br />
-        <input type="text1" />
+        <input
+          value={topText}
+          onChange={(event) => setTopText(event.currentTarget.value)}
+        />
       </label>
+      <br />
       <br />
       <label>
         Bottom meme Text
         <br />
-        <input type="text2" />
+        <input
+          value={bottomText}
+          onChange={(event) => setBottomText(event.currentTarget.value)}
+        />
       </label>
+      <button
+        onClick={() => {
+          setUrl(
+            `https://api.memegen.link/images/${images}/${topText}/${bottomText}.jpg`,
+          );
+        }}
+      >
+        Generate
+      </button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
