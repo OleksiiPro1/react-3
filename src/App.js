94ae7baa2c755a838d8ec0587ff8a9d9277057c9
Alexey Piltenko <ukr.top@ukr.net>
2022-05-12 16:09:01 +0300

    ok, I understand something))

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 
-const hStyle = { color: 'red' };
-function App() {
-  useEffect(() => {
-    fetch('https://api.memegen.link/templates')
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  }, []);
+const hStyle = { color: 'green' };
+export default function App() {
+  const meme = {
+    name: 'Meme generator',
+    profileImageUrl: 'https://api.memegen.link/images/aag/foo/bar.png',
+  };
+  const templates = [
+    { id: 1, pic: 'aag' },
+    { id: 2, pic: 'ackbar' },
+    { id: 3, pic: 'afraid' },
+    { id: 4, pic: 'agnes' },
+  ];
 
   return (
     <div className="App">
-      <h1 style={hStyle}>Meme generator</h1>
+      <img src={meme.profileImageUrl} alt="My meme generator" />
+      <h1 style={hStyle}>{meme.name}</h1>
+      {templates.map((template) => (
+        <select key={template.id}>
+          {template.id}
+          {template.pic}
+        </select>
+      ))}
+
       <label>
         Top meme Text
         <br />
-        <input type="text" />
+        <input type="text1" />
       </label>
       <br />
       <label>
         Bottom meme Text
         <br />
-        <input type="text" />
+        <input type="text2" />
       </label>
     </div>
   );
 }
-
-export default App;
